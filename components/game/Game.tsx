f3b76d49bc0d5d48804cd75bb257a4a97482c035
cboenke <clara.boenke@web.de>
2021-03-28 13:53:46 +0200

    ✨ Add niffler as player with keyboard navigation

--- components/game/Game.tsx
+++ components/game/Game.tsx
@@ -17,13 +17,15 @@ function Game() {
       },
     };
 
+    let player;
+
     function preload() {
       this.load.image("street", "/canvasLayer1.svg");
       this.load.image("granny", "/granny.png");
       this.load.image("purse1", "/purse1.svg");
       //   this.load.image("purse2", "/purse2");
       this.load.spritesheet("niffler", "/spritesheetNiffler.png", {
-        frameWidth: 121.5,
+        frameWidth: 120,
         frameHeight: 51,
       });
     }
@@ -32,10 +34,47 @@ function Game() {
       this.add.image(-350, 52, "street");
       this.add.image(212, 140, "granny");
       this.add.image(247, 224, "purse1");
+
+      player = this.physics.add.sprite(80, 280, "niffler");
+
+      player.setBounce(0);
+      // player.setVelocity(70, 0);
+      player.setCollideWorldBounds(true);
+
+      this.anims.create({
+        key: "standing",
+        frames: this.anims.generateFrameNumbers("niffler", {
+          start: 0,
+          end: 0,
+        }),
+      });
+
+      this.anims.create({
+        key: "running",
+        frames: this.anims.generateFrameNumbers("niffler", {
+          start: 0,
+          end: 1,
+        }),
+        frameRate: 10,
+        repeat: -1,
+      });
     }
 
     function update() {
-      console.log("This function will be edited later.");
+      const cursors = this.input.keyboard.createCursorKeys();
+      if (cursors.right.isDown) {
+        player.setVelocityX(100);
+        player.anims.play("running", true);
+      } else if (cursors.up.isDown) {
+        player.setVelocityY(-50);
+        player.anims.play("running", true);
+      } else if (cursors.down.isDown) {
+        player.setVelocityY(50);
+        player.anims.play("running", true);
+      } else {
+        player.setVelocityX(0) && player.setVelocityY(0);
+        player.anims.play("standing", true);
+      }
     }
 
     const game = new Phaser.Game(config);
