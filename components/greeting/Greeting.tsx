caefa4ea195d8272c6ce612b38b531577f900ff4
cboenke <clara.boenke@web.de>
2021-04-09 18:10:59 +0200

    ✨ Add game name to welcome page

--- components/greeting/Greeting.tsx
+++ components/greeting/Greeting.tsx
@@ -3,7 +3,7 @@ import styles from "./Greeting.module.css";
 function Greeting() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Welcome to ****</h1>
+      <h1 className={styles.heading}>Welcome to GoldNiffler</h1>
       <h2 className={styles.subheading}>A Niffler Fangame</h2>
     </div>
   );
